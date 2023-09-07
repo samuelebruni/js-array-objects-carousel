6bce5fc4982cc8b4a83b167221e6dd4659180b89
samuelebruni <samuele_bruni@hotmail.com>
2023-09-07 16:51:23 +0200

    end exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,23 @@
 
 
 const slides = [
-  './assets/img/01.webp', 
-  './assets/img/02.webp',
-  './assets/img/03.webp', 
-  './assets/img/04.webp',
-  './assets/img/05.webp',
-]
+  {
+    sorgente: './assets/img/01.webp',
+  },
+  {
+    sorgente: './assets/img/02.webp',
+  },
+  {
+    sorgente: './assets/img/03.webp',
+  },
+  {
+    sorgente: './assets/img/04.webp',
+  },
+  {
+    sorgente: './assets/img/05.webp',
+  }
+];
+
 
 let activeSlide = 0;
 
@@ -15,22 +26,16 @@ const sliderImagesEl = document.querySelector('.slider .images')
 const prevEl = document.querySelector('.prev')
 const nextEl = document.querySelector('.next')
  
-for (let i = 0; i < slides.length; i++) {
-  const slidePath = slides[i];
-  const slideMarkup = `<img class="${activeSlide === i ? 'active' : '' }" src="${slidePath}" alt="">`
-  sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
-}
-
-const slidesImages = document.querySelectorAll('.slider .images > img')
-console.log(slidesImages);
-
 const thumbsElement = document.querySelector('.thumbnails')
 
-for (let i = 0; i < slides.length; i++) {
-  const thumbPath = slides[i];
-  const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="${thumbPath}" alt="">`
-  thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
-}
+slides.forEach((slide, index) => {
+  const slideMarkup = `<img class="${activeSlide === index ? 'active' : '' }" src="${slide.sorgente}" alt="">`;
+  const thumbMarkup = `<img class="thumb ${activeSlide === index ? 'active' : ''}" src="${slide.sorgente}" alt="">`;
+  sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup);
+  thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup);
+}) 
+
+const slidesImages = document.querySelectorAll('.slider .images > img')
 
 nextEl.addEventListener('click', scrollImg);
 
@@ -48,7 +53,7 @@ document.getElementById('reverse').addEventListener('click', function (){
 
 
 
-
+//__________________________________________________________
 
 
 function scrollImg() {
